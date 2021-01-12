3989e6c8573f473f04bf246249b5d9243a4c4809
Mason Fisher <sackzombie10@gmail.com>
2021-01-12 14:31:32 -0500

    Made the web server return a page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 var http = require('http');
 const port = 8000; // The port used for this HTTP server
 
-var server = http.createServer(function(request, response) {
-  // Server code goes here
+var server = http.createServer(function(req, res) {
+  /*
+  req - Request
+  res - Response
+  */
+  if (req.url == '/') {
+    res.writeHead(200, {'Content-Type':"text/html"});
+    res.write('<html><head><title>Hello, world</title></head><body><h1>Hello, world!</h1></body></html>');
+    res.end();
+  }
 });
 
 server.listen(port); // Listen for any incoming requests
